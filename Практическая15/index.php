39959c0c4d6a74f89bea2a0d358edca92f10c9ed
MetalMelkiy <metalmelkiy@gmail.com>
2017-03-22 13:31:43 +0300

    php

--- Практическая15/index.php
+++ Практическая15/index.php
@@ -4,12 +4,21 @@
   <title>test PHP</title>
  </head>
  <body>
+ <form method="post">
+ <input type="text" name="num1" pattern="[0-9]{4}" value="1111" required="required">
+ <input type="text" name="num2" pattern="[0-9]{4}" value="1111" required="required">
+ <input type="text" name="num3" pattern="[0-9]{4}" value="1111" required="required">
+ <input type="text" name="num4" pattern="[0-9]{4}" value="1666" required="required">
+ <input type="submit">
+ </form>
  <?php
+ $creditcardIn=$_POST['num1'].=$_POST['num2'].=$_POST['num3'].=$_POST['num4'];
 function numhide(string $creditcard){
+	 
 
 	echo "************",substr($creditcard, 12, 16);
 }
-numhide("1234567890121488");
+numhide($creditcardIn);
 ?>
  </body>
 </html>
